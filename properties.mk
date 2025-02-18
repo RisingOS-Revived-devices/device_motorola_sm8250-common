488ebf0a81559ee362f4c8df77da7158ee4f32d3
Jyotiraditya <jyotiraditya@aospa.co>
2025-02-18 11:38:19 +0000

    sm8250-common: props: Enable config_avoidGfxAccel
    - This flag turns off GPU/CPU intensive UI effects and
      reduces the RAM footprint.
    
    - Using this is a better compromise than the 'low_ram' flag
      as this does not disable features in the rom but still
      reduces RAM usage and gives better smoothness in the UI.
    
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: Sanjivns <alshahab109@gmail.com>

--- properties.mk
+++ properties.mk
@@ -166,6 +166,7 @@ vendor.audio.feature.anc_headset.enable=false \
 vendor.audio.feature.battery_listener.enable=true \
 vendor.audio.feature.compr_cap.enable=false \
 vendor.audio.feature.compress_in.enable=true \
+ro.config.avoid_gfx_accel=true \
 vendor.audio.feature.compress_meta_data.enable=true \
 vendor.audio.feature.compr_voip.enable=true \
 vendor.audio.feature.concurrent_capture.enable=true \
