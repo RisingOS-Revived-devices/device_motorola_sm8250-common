29d9be7602fc509b50580d53365c2c6937911454
Sato-goj <kaloresanket@gmail.com>
2025-05-18 07:56:16 +0530

    sm8250-common: Build ViperFx

--- common.mk
+++ common.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Viper
+$(call inherit-product, hardware/motorola/ViPER4AndroidFX/setup.mk)
+
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
